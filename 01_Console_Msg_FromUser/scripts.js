86296a40b613f98211c24fc15cd2b39f77dc647c
Paratella <152934047+devParatella@users.noreply.github.com>
2024-02-20 01:48:24 -0300

    Add files via upload

---
+++ 01_Console_Msg_FromUser/scripts.js
@@ -0,0 +1,6 @@
+
+// Solicita ao usuário que digite uma mensagem
+var mensagemUsuario = prompt("Digite uma mensagem:");
+
+// Exibe a mensagem no console
+console.log("Mensagem do usuário:", mensagemUsuario);
